7bb312962510f998edbdd896fa1e83a937621b5b
ozancanguz <ozancanguz24@gmail.com>
2023-09-28 13:54:52 +0300

    screens created

---
+++ app/src/main/java/com/example/comicslbrarycompose/view/CharacterDetailScreen.kt
@@ -0,0 +1,11 @@
+package com.example.comicslbrarycompose.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun CharacterDetailScreen() {
+    
+   Text(text = "CHaracter details screen")
+}
---
+++ app/src/main/java/com/example/comicslbrarycompose/view/LibraryScreen.kt
@@ -0,0 +1,11 @@
+package com.example.comicslbrarycompose.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LibraryScreen() {
+
+
+    Text(text = "LİBRARY SCREEN")
+}
